1aa3032ddc3bf96932c0273d3731859b70fb6ca2
lilgreenbird <v-susanh@microsoft.com>
2021-07-12 18:29:58 -0700

    fix

--- src/test/java/com/microsoft/sqlserver/jdbc/connection/SSLProtocolTest.java
+++ src/test/java/com/microsoft/sqlserver/jdbc/connection/SSLProtocolTest.java
@@ -45,12 +45,13 @@ public class SSLProtocolTest extends AbstractTest {
             // Some older versions of SQLServer might not have all the TLS protocol versions enabled.
             // Example, if the highest TLS version enabled in the server is TLSv1.1,
             // the connection will fail if we enable only TLSv1.2
-            assertTrue(e.getMessage().contains(TestResource.getResource("R_noProtocolVersion"))
-                    || null != e.getCause()
-                                            ? e.getCause().getMessage()
-                                                    .contains(TestResource.getResource("R_connectionClosed"))
-                                            : null != e.getCause().getCause() ? e.getCause().getCause().getMessage()
-                                                    .contains(TestResource.getResource("R_connectionClosed")) : true,
+            assertTrue(
+                    e.getMessage().contains(TestResource.getResource("R_noProtocolVersion")) || null != e.getCause() ? e
+                            .getCause().getMessage().contains(TestResource.getResource("R_connectionClosed"))
+                            || null != e.getCause().getCause()
+                                                               ? e.getCause().getCause().getMessage().contains(
+                                                                       TestResource.getResource("R_connectionClosed"))
+                                                               : false : false,
                     e.getMessage());
         }
     }
